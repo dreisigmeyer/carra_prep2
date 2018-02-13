fdd304b02320f17228e3cc4f54471be73b901e57
dreis002 <dreis002@app5.ces.census.gov>
2018-02-13 11:00:13 -0500

    adding create_GBD_metadata

---
+++ create_GBD_metadata/__main__.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import os, shutil, subprocess
+
+base_dir = "create_GBD_metadata"
+cw_dir = os.getcwd()
+fp_dir = cw_dir + "/" + base_dir
+
+print "Creating City misspellings file..."
+cd_str = """{FP}/pythonCode/cleanCityNames/""".format(FP = fp_dir)
+os.chdir(cd_str)
+subprocess.call(["perl", "cleanCityNames.pl"])
+print "Finished"
+
+print "Creating Inventor names file..."
+cd_str = """{FP}/pythonCode/inventorNames/""".format(FP = fp_dir)
+os.chdir(cd_str)
+subprocess.call(["perl", "inventorNames.pl"])
+print "Finished"
+
+os.chdir(cw_dir)
+print "Creating Zip Codes file..."
+process_data = """
+python -m {BD}.pythonCode.zip3Data.usgs_geonames.process_data
+""".format(BD = base_dir)
+os.system(process_data)
+
+parse_post_offices = """
+python -m {BD}.pythonCode.zip3Data.parse_post_offices \
+post_offices/
+""".format(BD = base_dir)
+os.system(parse_post_offices)
+
+parser = """
+python -m {BD}.pythonCode.zip3Data.parser \
+sparql_query_results.csv \
+infobox_properties_en.nt
+""".format(BD = base_dir)
+os.system(parser)
+
+zip3_cities = """
+python -m {BD}.pythonCode.zip3Data.zip3_cities
+""".format(BD = base_dir)
+os.system(zip3_cities)
+
+city_xslt = """
+python -m {BD}.pythonCode.zip3Data.city_xslt
+""".format(BD = base_dir)
+os.system(city_xslt)
+
+shutil.move(base_dir + "/pythonCode/inventorNames/inventors.xml", base_dir)
+shutil.move(base_dir + "/pythonCode/cleanCityNames/cityMisspellings.xml", base_dir)
+shutil.move(base_dir + "/pythonCode/zip3Data/zip3_cities.xml", base_dir + "/UTF8_zip3_cities.xml")
+os.remove(base_dir + "/pythonCode/zip3Data/us_towns.csv")
+os.remove(base_dir + "/pythonCode/zip3Data/post_office_zips.csv")
+os.remove(base_dir + "/pythonCode/zip3Data/usgs_zcta5.csv")
+os.remove(base_dir + "/pythonCode/zip3Data/hold_zip3_cities.xml")
+
+zip3_final = """
+iconv -f UTF8 -t ASCII//TRANSLIT < {BD}/UTF8_zip3_cities.xml > {BD}/ASCII_zip3_cities.xml
+""".format(BD = base_dir, CD = cw_dir)
+os.system(zip3_final)
+print "Finished"
+
---
+++ create_GBD_metadata/pythonCode/zip3Data/usgs_geonames/process_data.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+import codecs, os
+from ...helperFunctions import helper_funs as HF
+
+file_dir = os.path.dirname(os.path.relpath(__file__))
+os.chdir(file_dir)
+
+"""
+This is data from http://geonames.usgs.gov/domestic/download_data.htm "All Names" and gives us:
+
+FEATURE_ID|FEATURE_NAME
+
+** FEATURE_ID: 	
+Permanent, unique feature record identifier and official feature name as defined in INCITS 446-2008
+** FEATURE_NAME:	
+Permanent official feature name as defined in INCITS 446-2008 as well as alternate spellings
+"""
+alias_cmd = """
+awk -F'|' -v OFS='|' '{print $1, $2}' AllNames_* > alternate_names.csv && \
+sed -i '1d' alternate_names.csv && \
+sed -i 's/Census Designated Place//g' alternate_names.csv
+"""
+os.system(alias_cmd);
+
+"""
+This is data from geonames.usgs.gov/domestic/download_data.htm "AllStateFedCodes" and gives us:
+
+GEOID|FEATURE_ID|STATE_ALPHA|FEATURE_NAME|FIPS
+
+** STATE_NUMERIC (State FIPS) + CENSUS_CODE (Place FIPS):
+The unique two number code for a US State as specified in INCITS 38:200x, (Formerly FIPS 5-2)
+The unique two number code and the unique two letter alphabetic code for a US State as specified in 
+INCITS 38:200x, (Formerly FIPS 5-2)
+The two combined give us the GEOID (see below)
+** FEATURE_ID: 	
+Permanent, unique feature record identifier and official feature name as defined in INCITS 446-2008
+** STATE_ALPHA: 	
+The unique two letter alphabetic code for a US State as specified in INCITS 38:200x, (Formerly FIPS 5-2) 
+** FEATURE_NAME:	
+Permanent official feature name as defined in INCITS 446-2008
+** STATE_NUMERIC (State FIPS) + COUNTY_NUMERIC (County FIPS):
+The unique two number code for a US State as specified in INCITS 38:200x, (Formerly FIPS 5-2)
+The unique three number code for a county or county equivalent as specified in INCITS 31:200x, 
+(Formerly FIPS 6-4)
+Combined these give us the unique FIPS code for the county
+"""
+names_cmd = """
+awk -F'|' -v OFS='|' '{print $8 $4, $1, $9, $2, $8 $11}' ./states/*_FedCodes_* > codes_names_states.csv && \
+sed -i '1d' codes_names_states.csv && \
+sed -i 's/Census Designated Place//g' codes_names_states.csv
+"""
+os.system(names_cmd);
+
+"""
+This is data from www.census.gov/geo/maps-data/data/relationship.html "ZCTA Relationship File" 
+and gives us:
+
+GEOID|ZCTA5
+
+** GEOID: 	
+See STATE_NUMERIC (State FIPS) + CENSUS_CODE (Place FIPS) above
+** ZCTA5: 	
+Census ZCTA for 2010
+"""
+zcta5_cmd = """
+awk -F',' -v OFS='|' '{print $5, $1}' zcta_place_rel_10.txt > geoid_to_zcta5_2010.csv && \
+sed -i '1d' geoid_to_zcta5_2010.csv && \
+sed -i 's/Census Designated Place//g' geoid_to_zcta5_2010.csv
+"""
+os.system(zcta5_cmd);
+
+encoding = 'utf-8'
+
+in_file = codecs.open("geoid_to_zcta5_2010.csv", "r", encoding=encoding)
+geoid_to_zcta5 = {} # We need the geoids to link up different files
+for line in in_file:    
+    geoid, zcta5 = line.split('|')
+    geoid_to_zcta5[geoid] = zcta5[0:3]
+in_file.close()
+
+in_file = codecs.open("codes_names_states.csv", "r", encoding=encoding)
+geoid_info = {}
+fips_zip3s = {} # all of the zips in a county
+feature_to_geo_id = {} # also to link up different data files
+for line in in_file:
+    geoid, feature_id, state, name, fips = line.split('|')
+    clean_name = HF.clean_it(name)
+    if geoid in geoid_to_zcta5: # get all of the zip3s in a county
+        zip3 = geoid_to_zcta5[geoid]
+        try:
+            fips_zip3s[fips].update([zip3])
+        except Exception:
+            fips_zip3s[fips] = set([zip3])
+    feature_to_geo_id[feature_id] = geoid
+    geoid_info[geoid] = {}
+    geoid_info[geoid]['state'] = state
+    geoid_info[geoid]['name'] = set([clean_name])
+    geoid_info[geoid]['fips'] = fips
+in_file.close()
+
+in_file = codecs.open("alternate_names.csv", "r", encoding=encoding)
+for line in in_file:
+    feature_id, name = line.split('|')
+    clean_name = HF.clean_it(name)
+    if feature_id in feature_to_geo_id:
+        geoid = feature_to_geo_id[feature_id]
+        geoid_info[geoid]['name'].update([clean_name])
+in_file.close()
+
+out_file = codecs.open("usgs_zcta5.csv", "w", encoding=encoding)
+
+'''
+There were some major issues having things correctly save as utf-8 even
+though they had utf-8 characters in them.  Having a line early in the file
+with utf-8 characters in it took care of things?
+'''
+out_file.write(u"A UTF-8 character to enforce saving the file as UTF-8: Ñ\n")
+
+for geoid in geoid_info:
+    state = geoid_info[geoid]['state']
+    if geoid in geoid_to_zcta5:
+        zip3 = geoid_to_zcta5[geoid]
+        for name in geoid_info[geoid]['name']:
+            holder = '|'.join([name, state, zip3])
+            out_file.write(holder + "\n")
+    else:
+        fips = geoid_info[geoid]['fips']
+        if fips in fips_zip3s:
+            for zip3 in fips_zip3s[fips]:
+                for name in geoid_info[geoid]['name']:
+                    holder = '|'.join([name, state, zip3])
+                    out_file.write(holder + "\n")
+out_file.close()
+
+os.system("mv usgs_zcta5.csv ../")
+os.system("rm alternate_names.csv")
+os.system("rm codes_names_states.csv")
+os.system("rm geoid_to_zcta5_2010.csv")
