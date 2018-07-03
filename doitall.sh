b88137ceffee5021ee103d7662d7651b228631b4
efw <dwdreisigmeyer@icloud.com>
2018-07-03 11:09:09 -0400

    New JSON file staorage

---
+++ doitall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source activate carra_prep
+./get_uspto_data.sh
+cd GBD_1976_2001_dat_to_xml
+./runit.sh
+cd ../python_validation
+./runit.sh & PIDVAL=$!
+cd ../
+python -m create_GBD_metadata & PIDMETA=$!
+wait $PIDVAL
+wait $PIDMETA
+cp create_GBD_metadata/*.json CARRA_2014/parse_GBD/
+mv create_GBD_metadata/*.json ./
+cd CARRA_2014
+./run_it.sh
+cd ../patent_metadata
+./runit.sh
+cd ../
