cb59133f8bcf9a509f290a9a88daa4bea2051f8c
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-08-21 13:43:14 -0400

    changed README, removed Python environment and cleaned up code

--- doitall.sh
+++ doitall.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
+# The get_uspto_data.sh files should be executed independently and
+# then verified that all of the files were downloaded correctly.
+
+# create the 1976-2001 XML files
 cd dat_to_xml
-./get_uspto_data.sh
+# ./get_uspto_data.sh
 ./run_it.sh
+
+# clean up the 2002 and later XML files and gather together the
+# information needed for city misspellings, etc
 cd ../xml_rewrite
-./get_uspto_data.sh
+# ./get_uspto_data.sh
 ./run_it.sh & PIDVAL=$!
 cd ../
 python -m create_GBD_metadata & PIDMETA=$!
 wait $PIDVAL
 wait $PIDMETA
+
+# move data around
 # cp dat_to_xml/xml_files/*.bz2 for_carra/inData
 # cp xml_rewrite/rewriter/original_xml_files/*.bz2 for_carra/inData
 # cp create_GBD_metadata/*.json for_carra/parse_GBD/
 mv create_GBD_metadata/*.json ./
+
+# get the files ready to go to CARRA
 # cd for_carra
 # ./run_it.sh
 # rm messages
 # cd ../
+
+# get together some basic information for each patent
 cp dat_to_xml/xml_files/*.bz2 patent_metadata/inData
-cp xml_rewrite/rewriter/patent_metadata/*.bz2 for_carra/inData
+cp xml_rewrite/rewriter/original_xml_files/*.bz2 patent_metadata/inData
 cd patent_metadata
 ./run_it.sh
 cd ../
