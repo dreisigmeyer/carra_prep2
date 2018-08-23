18b6da05d90c0aaf15450cc9c008ff3c248695ae
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-08-23 14:12:46 -0400

    switching from cython to regular python module

--- for_carra/parse_GBD/__main__.py
+++ for_carra/parse_GBD/__main__.py
@@ -1,11 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import glob
+import json
 import os
+import random
+import sys
+import xmlParser
+from multiprocessing import Process
 
-NUMBER_OF_PROCESSES = 6
-base_dir = "parse_GBD"
-cw_dir = os.getcwd()
-fp_dir = cw_dir + "/" + base_dir
+
+def split_seq(seq, num_processes):
+    """
+    Slices a list into number_of_processes pieces
+    of roughly the same size
+    """
+    num_files = len(seq)
+    if num_files < num_processes:
+        num_processes = num_files
+    size = num_processes
+    newseq = []
+    splitsize = 1.0 / size * num_files
+    for i in range(size):
+        newseq.append(seq[int(round(i * splitsize)):int(round((i + 1) * splitsize))])
+    return newseq
+
+
+# Start processing
+number_of_processes = int(sys.argv[1])
 pathToData = 'inData/'
-launch_it = "python -u -m {BD}.launch {NP} {FP}".format(BD=base_dir, NP=NUMBER_OF_PROCESSES, FP=fp_dir)
-os.system(launch_it)
+pathToJSON = 'parse_GBD/'
+files = glob.glob(os.path.join(pathToData, "*.zip"))
+random.shuffle(files)  # Newer years have more granted patents
+files_list = split_seq(files, number_of_processes)
+if __name__ == '__main__':
+    with open(pathToJSON + 'zip3_cities.json') as json_data:
+        zips_dict = json.load(json_data)
+    with open(pathToJSON + 'cityMisspellings.json') as json_data:
+        cities_dict = json.load(json_data)
+    with open(pathToJSON + 'inventors.json') as json_data:
+        inventors_dict = json.load(json_data)
+    procs = []
+    for chunk in files_list:
+        p = Process(target=xmlParser.assign_zip3, args=(chunk, zips_dict, cities_dict, inventors_dict,))
+        procs.append(p)
+        p.start()
--- for_carra/run_it.sh
+++ for_carra/run_it.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-python parse_GBD/setup.py build_ext --inplace
-rm -rf build/
-python parse_GBD >> messages
+NUM_PY_THREADS=4
+python -m parse_GBD $NUM_PY_THREADS
 rm parse_GBD/*.json
 rm inData/*.zip
